bfe2ea3caf67d6a176838f536b7b15881788df61
Bjorn Low <Bjorn.Low@shell.com>
2023-12-06 18:31:36 +0100

    sync

---
+++ day5/day5.py
@@ -0,0 +1,87 @@
+import re
+import numpy as np
+
+with open(r"AoC2023\day5\day5_input.txt") as f:
+    # with open(r"AoC2023\day5\day5_inputsmall.txt") as f:
+    lines = f.readlines()
+
+
+def get_mapping_dict(src_start, dst_start, number):
+    d = {}
+    for i, j in zip(
+        range(src_start, src_start + number), range(dst_start, dst_start + number)
+    ):
+        d[i] = j
+    return d
+
+
+class MappingClass:
+    def __init__(self):
+        self.maps = {}
+
+    def add_range(self, start, length, delta):
+        def f(key):
+            if start <= key < start + length:
+                return key + delta
+            else:
+                return key
+
+        self.maps[start] = f
+
+    def __getitem__(self, key):
+        available_ranges = list(self.maps.keys())
+        applicable = [r for r in available_ranges if r < key]
+        if len(applicable) == 0:
+            return key
+        r = max(applicable)
+        return self.maps[r](key)
+
+
+seed2soil = MappingClass()
+soil2fert = MappingClass()
+fert2wat = MappingClass()
+wat2light = MappingClass()
+light2temp = MappingClass()
+temp2hum = MappingClass()
+hum2loc = MappingClass()
+
+input_map = {
+    "seed-to-soil map:": seed2soil,
+    "soil-to-fertilizer map:": soil2fert,
+    "fertilizer-to-water map:": fert2wat,
+    "water-to-light map:": wat2light,
+    "light-to-temperature map:": light2temp,
+    "temperature-to-humidity map:": temp2hum,
+    "humidity-to-location map:": hum2loc,
+}
+
+seeds = set()
+active_map = input_map["seed-to-soil map:"]
+
+for line in lines:
+    l = line.replace("\n", "")
+
+    if "seeds:" in l:
+        _, l = l.split(":")
+        seeds = set([int(s) for s in l.split(" ") if s != ""])
+    elif l == "":
+        continue
+    elif not l[0].isnumeric():
+        key = l
+    else:
+        dst_start, src_start, length = [int(x) for x in l.split(" ")]
+        input_map[key].add_range(src_start, length, dst_start - src_start)
+
+loc = np.inf
+for seed in seeds:
+    val = seed
+    val = input_map["seed-to-soil map:"][val]
+    val = input_map["soil-to-fertilizer map:"][val]
+    val = input_map["fertilizer-to-water map:"][val]
+    val = input_map["water-to-light map:"][val]
+    val = input_map["light-to-temperature map:"][val]
+    val = input_map["temperature-to-humidity map:"][val]
+    val = input_map["humidity-to-location map:"][val]
+    loc = min(loc, val)
+
+print(loc)
---
+++ day6/day6.py
@@ -0,0 +1,37 @@
+import re
+import numpy as np
+import math
+
+with open(r"AoC2023\day6\day6_input.txt") as f:
+    # with open(r"AoC2023\day6\day6_inputsmall.txt") as f:
+    lines = f.readlines()
+
+
+l = lines[0].replace("\n", "")
+_, times = l.split(":")
+times = [int(t) for t in times.split(" ") if t.isnumeric()]
+
+l = lines[1].replace("\n", "")
+_, distances = l.split(":")
+distances = [int(t) for t in distances.split(" ") if t.isnumeric()]
+
+
+def get_option_count(t, d):
+    sol1 = (1 / 2) * (t - math.sqrt(t**2 - 4 * d)) + 10 ** (-5)
+    sol2 = (1 / 2) * (t + math.sqrt(t**2 - 4 * d)) - +(10 ** (-5))
+
+    return min(math.floor(sol2), t) - max(math.ceil(sol1), 0) + 1
+
+
+total = 1
+for t, d in zip(times, distances):
+    options = get_option_count(t, d)
+    total *= options
+    # print(t, d, options)
+
+
+# print(total)
+
+# get_option_count(30, 200)
+
+print(get_option_count(38947970, 241154910741091))
