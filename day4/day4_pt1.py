c971b9320199c66d23552cdd6f06be53e1e6c1ae
bjorn low <bjorn.low@effem.com>
2024-12-05 08:05:13 +0100

    day4

--- day4/day4_pt1.py
+++ day4/day4_pt1.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 dirpath = Path("day4")
@@ -8,4 +7,77 @@ fname = dirpath / "day4.txt"
 with open(fname, "r") as f:
     lines = f.readlines()
 
-    
+lines = [l.replace("\n", "") for l in lines]
+
+next_letter = {"X": "M", "M": "A", "A": "S", "S": None}
+
+directions = [
+    "north",
+    "northeast",
+    "east",
+    "southeast",
+    "south",
+    "southwest",
+    "west",
+    "northwest",
+]
+
+
+def get_next_idx(direction, col_idx_ni, row_idx_ni):
+    if direction == "north":
+        col_idx_new = col_idx_ni
+        row_idx_new = row_idx_ni - 1
+    elif direction == "northeast":
+        col_idx_new = col_idx_ni + 1
+        row_idx_new = row_idx_ni - 1
+    elif direction == "east":
+        col_idx_new = col_idx_ni + 1
+        row_idx_new = row_idx_ni
+    elif direction == "southeast":
+        col_idx_new = col_idx_ni + 1
+        row_idx_new = row_idx_ni + 1
+    elif direction == "south":
+        col_idx_new = col_idx_ni
+        row_idx_new = row_idx_ni + 1
+    elif direction == "southwest":
+        col_idx_new = col_idx_ni - 1
+        row_idx_new = row_idx_ni + 1
+    elif direction == "west":
+        col_idx_new = col_idx_ni - 1
+        row_idx_new = row_idx_ni
+    elif direction == "northwest":
+        col_idx_new = col_idx_ni - 1
+        row_idx_new = row_idx_ni - 1
+
+    if (
+        col_idx_new < 0
+        or col_idx_new >= len(lines[0])
+        or row_idx_new < 0
+        or row_idx_new >= len(lines)
+    ):
+        return None, None
+    return col_idx_new, row_idx_new
+
+
+occurences = 0
+
+
+def find_xmas(direction, col_idx_fd, row_idx_fd):
+    for letter in "XMAS":
+        if col_idx_fd is None or row_idx_fd is None:
+            return 0
+        if lines[row_idx_fd][col_idx_fd] != letter:
+            return 0
+        col_idx_fd, row_idx_fd = get_next_idx(
+            direction=direction, col_idx_ni=col_idx_fd, row_idx_ni=row_idx_fd
+        )
+    return 1
+
+
+for col_idx in range(len(lines[0])):
+    for row_idx in range(len(lines)):
+        if lines[row_idx][col_idx] == "X":
+            for direction in directions:
+                occurences += find_xmas(direction, col_idx, row_idx)
+
+print(occurences)
--- day4/day4_pt2.py
+++ day4/day4_pt2.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 dirpath = Path("day4")
@@ -8,4 +7,27 @@ fname = dirpath / "day4.txt"
 with open(fname, "r") as f:
     lines = f.readlines()
 
-    
+lines = [l.replace("\n", "") for l in lines]
+
+
+occurences = 0
+
+
+def find_x_mas(col_idx, row_idx):
+    left_top = lines[row_idx - 1][col_idx - 1]
+    right_top = lines[row_idx - 1][col_idx + 1]
+    left_bottom = lines[row_idx + 1][col_idx - 1]
+    right_bottom = lines[row_idx + 1][col_idx + 1]
+
+    diagonal_letters = left_top + right_top + right_bottom + left_bottom
+    if diagonal_letters in ("MMSS", "MSSM", "SSMM", "SMMS"):
+        return 1
+    return 0
+
+
+for col_idx in range(1, len(lines[0]) - 1):
+    for row_idx in range(1, len(lines) - 1):
+        if lines[row_idx][col_idx] == "A":
+            occurences += find_x_mas(col_idx=col_idx, row_idx=row_idx)
+
+print(occurences)
